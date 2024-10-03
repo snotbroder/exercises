f1bb93550348bb01ef91360e83354f856c7280d5
Joris <jote0002@stud.kea.dk>
2024-10-03 12:11:07 +0200

    init

---
+++ language_switcher/js.js
@@ -0,0 +1,16 @@
+"use strict";
+const texts = {
+  de: {
+    texts: [
+      { text: "Das Bot", location: ".header" },
+      { text: "Das Ro-Bot", location: ".footer" },
+    ],
+  },
+  da: {
+    texts: [
+      { text: "Båden", location: ".header" },
+      { text: "Robotten", location: ".footer" },
+    ],
+  },
+};
+const locale = "da";
